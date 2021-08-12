dc5e24be91628e91752e6e544957ae3938179ea0
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2021-08-12 08:38:13 -0400

    Initial Commit

---
+++ HexClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HexClock
+//
+//  Created by Gustavo Anjos on 12/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HexClock/HexClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexClockApp.swift
+//  HexClock
+//
+//  Created by Gustavo Anjos on 12/08/21.
+//
+
+import SwiftUI
+
+@main
+struct HexClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
